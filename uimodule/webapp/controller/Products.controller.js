831068679f0a16d446cf3f9c1143f8958b958112
KatsuoAshimine <happy.sanny.sky@gmail.com>
2020-10-22 17:56:27 +0900

    first commit

---
+++ uimodule/webapp/controller/Products.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "tutorial/undefined/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("tutorial.undefined.controller.Products", {});
+});
